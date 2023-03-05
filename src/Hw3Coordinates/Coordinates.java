26b41880df54d369752840aed021d7abbeffab1e
NikolaiPekhotnyi <nikolaipekhotnyi@gmail.com>
2023-03-05 17:26:28 +0200

    Add coordinates

---
+++ src/Hw3Coordinates/Coordinates.java
@@ -0,0 +1,9 @@
+package Hw3Coordinates;
+
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude = 51.178976140837655;
+        double longitude = -1.825850221414654;
+        System.out.println("Stonehenge coordinates: " + "Latitude - " + latitude + ", " + "Longitude - " + longitude);
+    }
+}
